00eb4b5337dac5c28a3b5d054442b8fd5b923aaf
salenkoanton <salenko3110@gmail.com>
2016-12-12 20:18:49 +0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,9 +20,6 @@ APPEND_SLASH = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ers8=x61mg&b_wg607!7@vect&glk@xhr@4hx4fbnxee*_d^-p'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
